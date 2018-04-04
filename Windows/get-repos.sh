551e5c930a4544cab6d313898166395de27b61e8
Garrett <garrettwong@outlook.com>
2018-04-04 10:33:28 -0700

    get-repos

---
+++ Windows/get-repos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Script to get all repositories under a user from bitbucket
+#Usage: getAllRepos.sh [username]
+ 
+curl --insecure -u ${1} https://api.bitbucket.org/1.0/users/${1} > repoinfo
+# curl -u adomingues https://api.bitbucket.org/1.0/users/adomingues
+# cat repoinfo
+ 
+for repo_name in `cat repoinfo | sed -r 's/("name": )/\n\1/g' | sed -r 's/"name": "(.*)"/\1/' | sed -e 's/{//' | cut -f1 -d\" | tr '\n' ' '`
+do
+    echo "Cloning " $repo_name
+    git clone https://${1}@bitbucket.org/${1}/$repo_name
+    echo "---"
+done
